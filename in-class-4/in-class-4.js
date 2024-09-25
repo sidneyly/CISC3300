bd4abe7dd9f21d442c9f32001f2f13a682c74a69
sidneyly <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>
2024-09-25 16:59:36 -0400

    in-class-4

---
+++ in-class-4/in-class-4.js
@@ -0,0 +1,11 @@
+
+    console.log("in-class-4.js loaded!")
+
+    const numbers = [1,2,3,4,5];
+    for (let i = 0; i < numbers.length; i++) {
+        if (i % 2 === 0){
+            console.log(`${i} is even!`)
+        } else
+            console.log(`${i} is odd!`);
+      }; 
+
