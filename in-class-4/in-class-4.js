2b23fa2ac01489943c99b33869620e8f3d40f240
sidneyly <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>
2024-09-26 21:49:40 -0400

    in-class-4

--- in-class-4/in-class-4.js
+++ in-class-4/in-class-4.js
@@ -2,7 +2,7 @@
     console.log("in-class-4.js loaded!")
 
     const numbers = [1,2,3,4,5];
-    for (let i = 0; i < numbers.length; i++) {
+    for (let i = 0; i < numbers.length+1; i++) {
         if (i % 2 === 0){
             console.log(`${i} is even!`)
         } else
